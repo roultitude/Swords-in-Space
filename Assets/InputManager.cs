da0107b0ef56eb2ed4f4d0ac64f2024fa744d131
AngJunYang <handbombex3@gmail.com>
2022-05-18 20:39:59 +0800

    Implemented InputManager
    Added Interact key "F" for players, sending a RPC to server.

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -1,43 +1,90 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FishNet.Object;
+using FishNet.Connection;
 
-public class InputManager : MonoBehaviour
+public class InputManager : NetworkBehaviour
 {
     private List<Interactable> interactables;
+    private static Dictionary<NetworkConnection, List<Interactable>> data;
 
     // Start is called before the first frame update
     void Start()
     {
+        if (base.IsServer && data == null)
+        {
+            data = new Dictionary<NetworkConnection, List<Interactable>>();
 
+        }
+
+        if (base.IsServer)
+        {
+            interactables = new List<Interactable>();
+            data.Add(base.Owner, interactables);
+        }
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown("f"))
+        {
+            Interact();
+        }
 
+    }
+
+    void Interact()
+    {
+        if (!base.IsOwner) { return; }
+        InteractQuery(base.Owner);
+
+    }
+
+    [ServerRpc]
+    void InteractQuery(NetworkConnection conn = null)
+    {
+        Debug.Log("??");
+        ReplyInteractQuery(conn);
+    }
+
+    [TargetRpc]
+    void ReplyInteractQuery(NetworkConnection conn)
+    {
+        Debug.Log("Donkey");
     }
 
 
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (!base.IsServer) { return; }
+
+
         Interactable otherInteractable = other.gameObject.GetComponentInParent<Interactable>();
         if (otherInteractable)
         {
             interactables.Add(otherInteractable);
         }
-        Debug.Log(other.name);
-        Debug.Log(other.tag);
+
+
     }
+
+
 
     private void OnTriggerExit2D(Collider2D other)
     {
+        if (!base.IsServer) { return; }
+
+
         Interactable otherInteractable = other.gameObject.GetComponentInParent<Interactable>();
 
         if (otherInteractable)
         {
-               interactables.Remove(otherInteractable);
+            interactables.Remove(otherInteractable);
         }
 
     }
