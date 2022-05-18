588ab6eb6e65eafb7f1da65dd762457a868edc89
AngJunYang <handbombex3@gmail.com>
2022-05-18 14:49:33 +0800

    Setup Interactable Framework
    Drafted out a inheritance model for interactables and modules

---
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Interactable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public abstract void Interact(GameObject player);
+
+
+
+}
---
+++ Assets/Scripts/Interactables/Modules/Engines/Engine.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace SwordsInSpace
+{
+    public abstract class Engine : Module
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+};
