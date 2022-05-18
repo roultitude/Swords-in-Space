85d17e6ea129484548ee013ad7ec91720293c120
roultitude <51879825+roultitude@users.noreply.github.com>
2022-05-18 20:40:10 +0800

    Implemented Menu logic

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -2,14 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
-public class CameraManager : MonoBehaviour
-{
-    ICinemachineCamera virtualCamera;
-    // Start is called before the first frame update
-    void Start()
-    {
-        virtualCamera = Camera.main.GetComponent<CinemachineBrain>().ActiveVirtualCamera;
-        virtualCamera.Follow= transform;
-    }
 
+namespace SwordsInSpace
+{
+    public class CameraManager : MonoBehaviour
+    {
+        ICinemachineCamera virtualCamera;
+        // Start is called before the first frame update
+        void Start()
+        {
+            virtualCamera = Camera.main.GetComponent<CinemachineBrain>().ActiveVirtualCamera;
+            virtualCamera.Follow = transform;
+        }
+
+    }
 }
--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -3,38 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using FishNet.Object;
 
-public class PlayerMover : NetworkBehaviour
+namespace SwordsInSpace
 {
-    [SerializeField]
-    private float speed, rotationSpeed;
-
-    private Rigidbody2D rb;
-    private void Awake()
+    public class PlayerMover : NetworkBehaviour
     {
-        rb = GetComponent<Rigidbody2D>();
-    }
+        [SerializeField]
+        private float speed, rotationSpeed;
 
-    private void Update()
-    {
-        
-    }
-    private void FixedUpdate()
-    {
-        Move();
-    }
+        private Rigidbody2D rb;
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
 
-    private void Move()
-    {
-        if (!base.IsOwner) return; //guard for not owner
+        private void Update()
+        {
+            
+        }
+        private void FixedUpdate()
+        {
+            Move();
+        }
 
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
-        Vector2 moveXY = new Vector2(moveX, moveY).normalized * speed * Time.fixedDeltaTime;
-        if (moveXY.sqrMagnitude == 0) return;
-        Vector2 newPos = new Vector2(rb.transform.position.x + moveXY.x, rb.transform.position.y + moveXY.y);
-        rb.MovePosition(newPos);
-        float angle = Mathf.Atan2(moveY, moveX) * Mathf.Rad2Deg;
-        rb.transform.rotation = Quaternion.Slerp(rb.transform.rotation, Quaternion.AngleAxis(angle, Vector3.forward), Time.fixedDeltaTime * rotationSpeed);
+        private void Move()
+        {
+            if (!base.IsOwner) return; //guard for not owner
+
+            float moveX = Input.GetAxisRaw("Horizontal");
+            float moveY = Input.GetAxisRaw("Vertical");
+            Vector2 moveXY = new Vector2(moveX, moveY).normalized * speed * Time.fixedDeltaTime;
+            if (moveXY.sqrMagnitude == 0) return;
+            Vector2 newPos = new Vector2(rb.transform.position.x + moveXY.x, rb.transform.position.y + moveXY.y);
+            rb.MovePosition(newPos);
+            float angle = Mathf.Atan2(moveY, moveX) * Mathf.Rad2Deg;
+            rb.transform.rotation = Quaternion.Slerp(rb.transform.rotation, Quaternion.AngleAxis(angle, Vector3.forward), Time.fixedDeltaTime * rotationSpeed);
+        }
     }
-    
 }
