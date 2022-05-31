eaa9d4842090e6344be36ea85716226a942833b9
roultitude <51879825+roultitude@users.noreply.github.com>
2022-05-31 16:27:55 +0800

    Adjusted the camera damping

--- Assets/Scripts/Player/PlayerTracker.cs
+++ Assets/Scripts/Player/PlayerTracker.cs
@@ -7,14 +7,19 @@ namespace SwordsInSpace
 {
     public class PlayerTracker : NetworkBehaviour
     {
-
+        private Transform realPlayer;
         private void Update()
         {
             if (IsServerOnly) return;
             if (User.localUser)
             {
-                if (User.localUser.controlledPlayer) transform.localPosition = User.localUser.controlledPlayer.transform.localPosition;
+                if (User.localUser.controlledPlayer) realPlayer = User.localUser.controlledPlayer.transform;
             }
+            if(realPlayer) transform.localPosition
+                        = new Vector2(
+                            Mathf.Lerp(transform.localPosition.x,realPlayer.localPosition.x,0.03f),
+                            Mathf.Lerp(transform.localPosition.y, realPlayer.localPosition.y, 0.03f)
+                            );
         }
         public override void OnStartClient()
         {
